2b647ebe7dc1b09d3928b629341f865bd6d1a630
Geisa Lopes <geisa.lopes@icloud.com>
2024-09-24 10:35:26 +0200

    Start Chapter 5

---
+++ Chapter 5/alien_color.py
@@ -0,0 +1,4 @@
+alien_color = "teal"
+
+if alien_color == "teal":
+    print("You just earned 5 points!")
---
+++ Chapter 5/alien_colors_two.py
@@ -0,0 +1,14 @@
+alien_color = "teal"
+
+if alien_color == "teal":
+    print("You just earned 5 points!")
+else:
+    print("You just earned 10 points!")
+
+
+alien_color = "green"
+
+if alien_color == "teal":
+    print("You just earned 5 points!")
+else:
+    print("You just earned 10 points!")
