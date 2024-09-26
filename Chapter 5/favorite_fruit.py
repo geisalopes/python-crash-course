1ff58caf5e7ddc2f6065d0ec55bfa1eec9a2b827
Geisa Lopes <geisa.lopes@icloud.com>
2024-09-26 16:53:19 +0200

    Finish the exercise 5-8

---
+++ Chapter 5/favorite_fruit.py
@@ -0,0 +1,13 @@
+favorite_fruits = ["strawberry", "mandarine", "feig"]
+
+for fruit in favorite_fruits:
+    if fruit == "strawberry":
+        print("\nYou really like " + fruit + "!")
+    if fruit == "mandarine":
+        print("\nYou really like " + fruit + "!")
+    if fruit == "feig":
+        print("\nYou really like " + fruit + "!")
+    if fruit == "peach":
+        print("\nYou really like " + fruit + "!")
+    if fruit == "banana":
+        print("\nYou really like " + fruit + "!")
