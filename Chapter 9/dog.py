f399fcf34aed1bbd7bcbb0949a9e5e88de1e4c64
Geisa Lopes <geisa.lopes@icloud.com>
2024-10-17 14:24:14 +0200

    Star Chapter 9

---
+++ Chapter 9/dog.py
@@ -0,0 +1,15 @@
+class Dog:
+    """A simple attempt to model a dog."""
+
+    def __init__(self, name, age):
+        """Initialize name and age attributes."""
+        self.name = name
+        self.age = age
+
+    def sit(self):
+        """Simulate a dog sitting in response to a command."""
+        print(f"{self.name} is now sitting.")
+
+    def roll(self):
+        """Simulate a dog rolling in over response to a command"""
+        print(f"{self.name} rolled over!")
