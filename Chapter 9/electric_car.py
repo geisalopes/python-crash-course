d4bfcae241bf8fd88dee5fb5484b94f6a4c171c4
Geisa Lopes <geisa.lopes@icloud.com>
2024-10-22 11:15:55 +0200

    Learn the battery car example

--- Chapter 9/electric_car.py
+++ Chapter 9/electric_car.py
@@ -28,6 +28,27 @@ class Car:
         self.odometer_reading += miles
 
 
+class Battery:
+    """A simple attempt to model a battery for an electric car."""
+
+    def __init__(self, battery_size=45):
+        """Initialize the battery's attributes."""
+        self.battery_size = battery_size
+
+    def describe_battery(self):
+        """Print a statement describing the battery size."""
+        print(f"This car has a {self.battery_size}-kWh battery.")
+
+    def get_range(self):
+        """Print a statement about the range this battery provides."""
+        if self.battery_size == 40:
+            range = 150
+        elif self.battery_size == 65:
+            range = 225
+
+        print(f"This car can go about {range} miles on a full charge.")
+
+
 class ElectricCar(Car):
     """Represent aspects of a car, specific to electric vehicles."""
 
@@ -37,13 +58,10 @@ class ElectricCar(Car):
         Then initialize attributes specific to an electric car.
         """
         super().__init__(make, model, year)
-        self.battery_size = 40
-
-    def describe_battery(self):
-        """Print a statement describing the battery size."""
-        print(f"This car has a {self.battery_size}-kWh battery.")
+        self.battery = Battery()
 
 
 my_leaf = ElectricCar("nissan", "leaf", 2024)
 print(my_leaf.get_descriptive_name())
-my_leaf.describe_battery()
+my_leaf.battery.describe_battery()
+my_leaf.battery.get_range()
