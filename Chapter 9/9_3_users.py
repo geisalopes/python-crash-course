ce20f654a8978ccebd113eac089a851d216e771f
Geisa Lopes <geisa.lopes@icloud.com>
2024-11-12 16:22:56 +0100

    Redp exercise 9.3 Users

--- Chapter 9/9_3_users.py
+++ Chapter 9/9_3_users.py
@@ -1,26 +1,28 @@
 class User:
-    """Represent a simple profile"""
+    """A simple class to describe a user"""
 
-    def __init__(self, first_name, last_name, age, location, hobby):
-        """Initialize the user."""
-        self.first = first_name
-        self.last = last_name
-        self.username = self.first + " " + self.last
-        self.age = age
-        self.location = location
-        self.hobby = hobby
+    def __init__(self, first_name, last_name, username, email):
+        """Defining the user's attributes"""
+        self.first_name = first_name.title()
+        self.last_name = last_name.title()
+        self.username = username
+        self.email = email
 
     def describe_user(self):
-        """Display a summary of the user's information."""
-        print(f"\nHere is the informations about the user {self.username}:")
+        """A methos to describe the user"""
         print(
-            f"Age: {self.age} years old, Location: {self.location}, Hobby: {self.hobby}"
+            f"User's Information: Name: {self.first_name} {self.last_name}, Username: {self.username}, Email: {self.email}"
         )
 
     def greet_user(self):
-        print(f"\nHello, {self.username}! Welcome on board!\n")
+        """A method to welcome the user"""
+        print(f"Welcome {self.first_name} {self.last_name}.\n")
 
 
-user_one = User("Geisa", "Lopes", 40, "Berlin", "drawing")
-user_one.describe_user()
-user_one.greet_user()
+geisa = User("geisa", "lopes", "geisalopes", "geisa84@gmail")
+geisa.describe_user()
+geisa.greet_user()
+
+diogo = User("diogo", "eichert", "dgeichert", "deichert@gmail")
+diogo.describe_user()
+diogo.greet_user()
