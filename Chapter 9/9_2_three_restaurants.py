267056996143af9ed24c114068365381adc5b50e
Geisa Lopes <geisa.lopes@icloud.com>
2024-11-12 15:52:33 +0100

    Redo the exercise 9.2 Three Restaurants

--- Chapter 9/9_2_three_restaurants.py
+++ Chapter 9/9_2_three_restaurants.py
@@ -1,29 +1,23 @@
 class Restaurant:
-    """A class representing restaurant"""
+    """A simple description of a restaurant"""
 
-    def __init__(self, restaurant_name, cuisine_type):
-        """Describing the restaurant"""
-        self.restaurant = restaurant_name
-        self.cuisine = cuisine_type
+    def __init__(self, name, cuisine):
+        """Initialize name and type attributes"""
+        self.name = name.title()
+        self.cuisine = cuisine.title()
 
     def describe_restaurant(self):
-        print(
-            f"The cuisine type of the restaurant {self.restaurant} is {self.cuisine}."
-        )
+        print(f"\nThe {self.name} is specialized in {self.cuisine}.")
 
     def open_restaurant(self):
-        print(f"The restaurant {self.restaurant} is open!\n")
+        print(f"\nThe {self.name} is open. Welcome!")
 
 
-asian_restaurant = Restaurant("Chai Umy", "asian fusion")
-italian_restaurant = Restaurant("Da Ponte", "italian")
-fast_food_restaurant = Restaurant("Swing Kitchen", "fast food")
+thai_restaurant = Restaurant("chay umi", "thai food")
+thai_restaurant.describe_restaurant()
 
-asian_restaurant.describe_restaurant()
-asian_restaurant.open_restaurant()
-
+italian_restaurant = Restaurant("the sanctuary", "croissants")
 italian_restaurant.describe_restaurant()
-italian_restaurant.open_restaurant()
 
-fast_food_restaurant.describe_restaurant()
-fast_food_restaurant.open_restaurant()
+donuts_restaurant = Restaurant("brammibal's", "vegan donuts")
+donuts_restaurant.describe_restaurant()
