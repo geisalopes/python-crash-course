205a0f8fc10072fac472037e4150e23477fefaec
Geisa Lopes <geisa.lopes@icloud.com>
2025-02-21 11:20:40 +0100

    Exercises

--- Chapter 9/book_and_author.py
+++ Chapter 9/book_and_author.py
@@ -1,21 +1,41 @@
-class Movie:
-"""Represents a movie."""
+class Author:
+    """A simple Author for a book"""
 
-    def __init__(self, title, year):
-        self.title = title
-        self.year = year
-
-    def describe_movir (self):
-        print(f"My favorite actor are {self.title}, released in {self.year}.")
-
-class Actor:
-    def __init__(self, name, movies):
+    def __init__(self, name, birth_year):
         self.name = name
-        self.movies = movies
+        self.birth_year = birth_year
 
-    def list_movies(self):
-        print(f"{self.name} made the following movies: {self.movies}")
+    def describe_author(self):
+        """Display a message about the author"""
+        print(f"The author of this book is {self.name}, born in {self.birth_year}.")
+
+    def get_age(self, current_year):
+        """Display the author's age based on the current year."""
+        age = current_year - self.birth_year
+        print(f"{self.name} would be approximately {age} years old today.")
 
 
-my_movie = Movie("Love Actually, 2005")
-my_actor = Actor("Alan, Rickman,")
+class Book:
+    """Represents specific aspects of a book."""
+
+    def __init__(self, title, genre, publication_year):
+        """Initialize the books' attributes."""
+        self.title = title
+        self.genre = genre
+        self.publication_year = publication_year
+        self.author = Author("Machado de Assis", 1839)
+
+    def describe_book(self):
+        """Display information about a book."""
+        print(
+            f"{self.title}, a book of the genre {self.genre}, published in {self.publication_year}."
+        )
+
+
+my_book = Book("Dom Casmurro", "Novel", 1899)
+
+my_book.describe_book()
+
+my_book.author.describe_author()
+
+my_book.author.get_age(2025)
--- Chapter 9/fav_actor.py
+++ Chapter 9/fav_actor.py
@@ -1,44 +1,23 @@
 class Movie:
-    """Represents a movie."""
-
     def __init__(self, title, year):
         self.title = title
         self.year = year
 
-    def describe_movie(self):
-        print(f"The movie '{self.title}' was released in {self.year}.")
 
-
-class Actor:
-    """Represents an actor with a list of movies."""
-
-    def __init__(self, name):
+class Actress:
+    def __init__(self, name, movie):
         self.name = name
-        self.movies = []  # Inicializa uma lista vazia de filmes
+        self.movie = movie
 
-    def add_movie(self, movie):
-        """Adds a movie (instance of Movie) to the actor's filmography."""
-        self.movies.append(movie)
-
-    def list_movies(self):
-        """Prints all movies the actor has starred in."""
-        print(f"\n{self.name} starred in the following movies:")
-        for movie in self.movies:
-            print(f"- {movie.title} ({movie.year})")
+    def describe_actress(self):
+        print(f"{self.name} starred in '{self.movie.title}' ({self.movie.year}).")
 
 
-# Criando alguns filmes
-movie1 = Movie("Love Actually", 2003)
-movie2 = Movie("Die Hard", 1988)
-movie3 = Movie("Harry Potter and the Philosopher's Stone", 2001)
+movie1 = Movie("Mamma Mia!", 2008)
+movie2 = Movie("Alien", 1979)
 
-# Criando o ator Alan Rickman
-alan_rickman = Actor("Alan Rickman")
+actress1 = Actress("Meryl Streep", movie1)
+actress2 = Actress("Sigourney Weaver", movie2)
 
-# Adicionando filmes à filmografia do ator
-alan_rickman.add_movie(movie1)
-alan_rickman.add_movie(movie2)
-alan_rickman.add_movie(movie3)
-
-# Exibindo os filmes do ator
-alan_rickman.list_movies()
+actress1.describe_actress()
+actress2.describe_actress()
