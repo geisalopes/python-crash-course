e31d959fdfa05ce7c782f4b3319e90398ab3d315
Geisa Lopes <geisa.lopes@icloud.com>
2025-03-26 12:47:01 +0100

    10-7: Addition Calculator

--- Chapter 10/10_6_addition.py
+++ Chapter 10/10_6_addition.py
@@ -1,8 +1,21 @@
-try:
-    number_one = int(input("Give me one number. "))
-    number_two = int(input("Give me another number. "))
-except ValueError:
-    print("Please, give a valid number.")
-else:
-    numbers = number_one + number_two
-    print(f"The sum of the numbers is: {numbers}.")
+print("Enter 'q' at any time to quit.\n")
+
+while True:
+    try:
+        number_one = input("\nGive me a number. ")
+        if number_one == "q":
+            break
+
+        number_one = int(number_one)
+
+        number_two = input("\nGive me another number. ")
+        if number_two == "q":
+            break
+
+        number_two = int(number_two)
+
+    except ValueError:
+        print("Please, give a valid number.")
+    else:
+        numbers = number_one + number_two
+        print(f"The sum of the numbers is: {numbers}.")
