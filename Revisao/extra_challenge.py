fb4e7f9d5cf4582a17708e05218c1a4424375765
Geisa Lopes <geisa.lopes@icloud.com>
2025-05-19 17:03:00 +0200

    Start Chapter 11

--- Revisao/extra_challenge.py
+++
@@ -1,9 +0,0 @@
-full_name = "geisa" + " " + "lopes"
-profession = "Python Developer"
-city = "Berlin"
-
-print("--------------------------")
-print(f"Name: {full_name.title()}")
-print(f"Profession: {profession}")
-print(f"City: {city}")
-print("--------------------------")
