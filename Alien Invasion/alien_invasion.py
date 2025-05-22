4ed873894bf3a80de3cffcfda9eddaf738e94875
Geisa Lopes <geisa.lopes@icloud.com>
2025-05-22 16:12:53 +0200

    Create the game screen

--- Alien Invasion/alien_invasion.py
+++ Alien Invasion/alien_invasion.py
@@ -10,13 +10,13 @@ class AlienInvasion:
         """Initialize the game, and create game resources."""
         pygame.init()
 
-        self.screen = pygame.display.set.mode((1200, 800))
+        self.screen = pygame.display.set_mode((1200, 800))
         pygame.display.set_caption("Alien Invasion")
 
     def run_game(self):
         """Start the main loop for the game."""
         while True:
-            # Watch for keyboard and moouse events.
+            # Watch for keyboard and mouse events.
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
